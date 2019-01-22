1cad0bad58ab23a91b7d663f92a433703081e367
egbertwong <651020224@qq.com>
2019-01-22 22:18:18 +0800

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: EgbertW
+ * @Description:
+ * @Date: Created in 22:07 2019/1/22
+ */
+public class main {
+    public static void main(String args[]) {
+        System.out.println("hahaha");
+    }
+}
